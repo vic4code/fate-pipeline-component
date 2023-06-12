30134aa0e7b662586409da7729542590bfc03ec5
林子傑 <jasper881108@gmail.com>
2023-06-12 14:22:47 +0800

    adjust

--- Jasper_dev/sync_persistence_from_local.sh
+++ Jasper_dev/sync_persistence_from_local.sh
@@ -1,4 +1,6 @@
 echo "========Sync Previous Data========"
+sudo kubectl exec client-0 -n fate-9999 -- sh -c 'rm -rf /data/projects/fate/persistence/*'
 sudo kubectl cp persistence fate-9999/client-0:/data/projects/fate
+sudo kubectl exec client-0 -n fate-10000 -- sh -c 'rm -rf /data/projects/fate/persistence/*'
 sudo kubectl cp persistence fate-10000/client-0:/data/projects/fate
 echo "========Use command to check pod inside [ sudo kubectl exec -it client-0 -n fate-9999 bash ]========="
--- Jasper_dev/sync_persistence_from_pod.sh
+++ Jasper_dev/sync_persistence_from_pod.sh
@@ -1,3 +1,4 @@
 echo "========Sync Previous Data========"
+sudo rm -r persistence
 sudo kubectl cp fate-9999/client-0:/data/projects/fate/persistence persistence
 echo "========Sync Done========="
